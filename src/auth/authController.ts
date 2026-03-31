dcb4e07f758b4d6765498fcd286d9b679646a554
root <root@vmi3163639.contaboserver.net>
2026-03-31 17:13:07 +0200

    chore: add placeholder auth controller (conflict test)

---
+++ src/auth/authController.ts
@@ -0,0 +1,12 @@
+// Placeholder auth controller - to be replaced
+export function login() {
+  throw new Error('Not implemented')
+}
+
+export function logout() {
+  throw new Error('Not implemented')
+}
+
+export function getMe() {
+  throw new Error('Not implemented')
+}
