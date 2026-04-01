8ad75eca4127f365044b6e9078215cc4098c7baf
root <root@vmi3163639.contaboserver.net>
2026-04-01 09:32:17 +0200

    test: create conflict for T08 resolve test

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+// Middleware barrel export - base branch version
+export { authMiddleware } from './auth-middleware'
+export { csrfMiddleware } from './csrf-middleware'
+export { rateLimiter } from './rate-limiter'
+export { roleGuard } from './role-guard'
