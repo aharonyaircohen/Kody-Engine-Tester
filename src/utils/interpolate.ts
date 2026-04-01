5ce1220bc75d886cbc61f6bc060e6c7cc7c3c7fd
Test <test@test.com>
2026-04-02 01:40:06 +0200

    Revert "test: add conflicting interpolate.ts for T08 conflict test"
    This reverts commit 9185adfe5772c779e2253c10cb97a580720f8172.

--- src/utils/interpolate.ts
+++
@@ -1,4 +0,0 @@
-// T08 conflict test - this should conflict with PR #296
-export function interpolate(template: string, values: Record<string, string>): string {
-  return template.replace(/\{(\w+)\}/g, (_, key) => values[key] ?? `{${key}}`);
-}
