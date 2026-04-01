5cd71932d78bc242dda30f13baae0b685041b360
root <root@vmi3163639.contaboserver.net>
2026-04-01 10:28:20 +0200

    Revert "test: create conflict for T08 resolve test"
    This reverts commit 8ad75eca4127f365044b6e9078215cc4098c7baf.

--- src/middleware/index.ts
+++
@@ -1,5 +0,0 @@
-// Middleware barrel export - base branch version
-export { authMiddleware } from './auth-middleware'
-export { csrfMiddleware } from './csrf-middleware'
-export { rateLimiter } from './rate-limiter'
-export { roleGuard } from './role-guard'
