64b474fd9a05095bc78081361a3e7378c4f6b11f
aguyaharonyair <aguy.aharon.yair@gmail.com>
2026-05-05 19:16:06 +0300

    feat(utils): add sleep helper for awaiting milliseconds

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,23 +1,3 @@
-export class TimeoutError extends Error {
-  constructor(ms: number) {
-    super(`Operation timed out after ${ms}ms`)
-    this.name = 'TimeoutError'
-  }
-}
-
 export function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-export async function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
-  let timeoutId: ReturnType<typeof setTimeout> | undefined
-  const timeout = new Promise<never>((_, reject) => {
-    timeoutId = setTimeout(() => reject(new TimeoutError(ms)), ms)
-  })
-
-  try {
-    return await Promise.race([promise, timeout])
-  } finally {
-    if (timeoutId !== undefined) clearTimeout(timeoutId)
-  }
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
