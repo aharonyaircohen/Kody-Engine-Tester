cba81b5340ea8c128ff74d0c0e019ea17facd32a
aguyaharonyair <aguy.aharon.yair@gmail.com>
2026-05-05 19:16:06 +0300

    feat(utils): add range helper to generate numeric sequences

--- src/utils/range.ts
+++ src/utils/range.ts
@@ -1,36 +1,5 @@
-export function range(startOrEnd: number, end?: number, step = 1): number[] {
-  let start: number
-
-  if (end === undefined) {
-    start = 0
-    end = startOrEnd
-  } else {
-    start = startOrEnd
-  }
-
-  if (step === 0) {
-    throw new Error('Step cannot be zero')
-  }
-
-  if (step > 0 && start > end) {
-    throw new Error('Step must be negative when start is greater than end')
-  }
-
-  if (step < 0 && start < end) {
-    throw new Error('Step must be positive when start is less than end')
-  }
-
-  const result: number[] = []
-
-  if (step > 0) {
-    for (let i = start; i < end; i += step) {
-      result.push(i)
-    }
-  } else {
-    for (let i = start; i > end; i += step) {
-      result.push(i)
-    }
-  }
-
-  return result
+export function range(start: number, end: number, step = 1): number[] {
+  const out: number[] = []
+  for (let i = start; i < end; i += step) out.push(i)
+  return out
 }
