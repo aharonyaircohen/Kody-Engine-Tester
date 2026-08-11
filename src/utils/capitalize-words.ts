5faeeaf673c36dc16fae61d4e1920e60d74baa75
aguyaharonyair <aguy.aharon.yair@gmail.com>
2026-08-11 19:26:59 +0300

    test(ci): trigger no-pr repair path

--- src/utils/capitalize-words.ts
+++ src/utils/capitalize-words.ts
@@ -1,7 +1,4 @@
 export function capitalizeWords(str: string): string {
   if (!str) return str
   return str
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ')
 }
